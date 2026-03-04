9688077192c73ec0a0129c4e11cf1d049ab5a136
babisha04 <babishariya@gmail.com>
2026-03-04 23:46:50 +0530

    Taxi Booking modeule

---
+++ CarBooking/TaxiBooking.java
@@ -0,0 +1,13 @@
+package CarBooking;
+
+import java.util.List;
+
+public class TaxiBooking {
+    
+    int taxiId;
+    char currentLocation;
+    int freeTime;
+    int totalEarnings;
+    List<Booking> trips;
+
+}
